10f5cbe1c8e642d387b7784289f2d24f879a10ae
Rohitjakkam <rohitjakkam@gmail.com>
2025-01-30 22:32:44 +0530

    Add news-signal service

---
+++ services/news-signal/llms/factory.py
@@ -0,0 +1,26 @@
+from typing import Literal
+
+from .base import BaseNewsSignalExtractor
+from .claude import ClaudeNewsSignalExtractor
+
+
+def get_llm(model_provider: Literal['anthropic', 'ollama']) -> BaseNewsSignalExtractor:
+    """
+    Returns the LLM we want for the news signal extractor
+    Args:
+        model_provider: The model provider to use
+    Returns:
+        The LLM we want for the news signal extractor
+    """
+    if model_provider == 'anthropic':
+        from .config import AnthropicConfig
+
+        config = AnthropicConfig()
+        return ClaudeNewsSignalExtractor(
+            model_name=config.model_name,
+            api_key=config.api_key,
+        )
+    # elif model_provider == "ollama":
+    #     return OllamaNewsSignalExtractor()
+    else:
+        raise ValueError(f'Unsupported model provider: {model_provider}')
